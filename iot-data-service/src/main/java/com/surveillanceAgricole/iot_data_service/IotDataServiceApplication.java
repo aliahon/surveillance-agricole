e49fb6788cef1d7c364f6f2d7c31f64e4a39890b
EL IDRISSI Nohaila <nohaila09el@gmail.com>
2025-02-28 11:05:49 +0000

    created a spring boot project for iot data service

---
+++ iot-data-service/src/main/java/com/surveillanceAgricole/iot_data_service/IotDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.surveillanceAgricole.iot_data_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotDataServiceApplication.class, args);
+	}
+
+}
